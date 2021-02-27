8ad53f6bdb4eba623dec45b49426478bb819fd7e
Kirill <benberov09@mail.ru>
2021-02-27 23:19:12 +0300

    Commit_master_1_27.02

---
+++ src/main/java/ru/geekbrains/javahibernate2/Javahibernate2Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.javahibernate2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javahibernate2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javahibernate2Application.class, args);
+    }
+
+}
