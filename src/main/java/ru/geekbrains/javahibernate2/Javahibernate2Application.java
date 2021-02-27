b5b740869a3e71641785ea66663a2f2c0694967e
Kirill <benberov09@mail.ru>
2021-02-28 00:27:11 +0300

    Commit_HW6_2_27.02

--- src/main/java/ru/geekbrains/javahibernate2/Javahibernate2Application.java
+++ src/main/java/ru/geekbrains/javahibernate2/Javahibernate2Application.java
@@ -1,13 +1,46 @@
 package ru.geekbrains.javahibernate2;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Javahibernate2Application {
 
     public static void main(String[] args) {
+
+        downloadSQLScript();
         SpringApplication.run(Javahibernate2Application.class, args);
+
     }
+
+    public static void downloadSQLScript() {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+        Session session = null;
+        try {
+            String sql = Files.lines(Paths.get("full.sql")).collect(Collectors.joining(" "));
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            factory.close();
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
 
 }
